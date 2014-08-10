ebe837e544d858f1ed160f89b5704357876d95f4
Gabe Roeloffs <gdroel@gmail.com>
2014-08-10 16:30:16 -0700

    interface changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,21 +7,21 @@
 </div>
 <div class="container">
 	<div class="row movedown">
-		<div class="col-md-4 info">
+		<div class="col-sm-4 info">
 			<h3>For Leaders:</h3>
 			<hr>
-			<h4>1. Sign up as a Leader</h4>
+			<h4>1. <a class="gray" href="{{ action('HomeController@showRegister') }}">Sign up</a> as a Leader</h4>
 			<h4>2. Add Your Students.</h4>
 			<h4>3. Lavish them with Gold Stars.</h4>
 		</div>
-		<div class="col-md-4 info">
+		<div class="col-sm-4 info">
 			<h3>Students:</h3>
 			<hr>
 			<h4>1. Search your Name.</h4>
 			<h4>2. Get your Stars.</h4>
 			<h4>3. Beat your Friends.</h4>
 		</div>
-		<div class="col-md-4 info">
+		<div class="col-sm-4 info">
 			<h3>Recent Users</h3>
 			<hr>
 			@if($users!="")
